7db5070eaee65103a75ca11727563fdc7e31937a
Utkarsh Karnwal <utkarsheons@gmail.com>
2026-02-23 16:05:41 +0530

    commit

---
+++ src/test/java/JavaCollectionsProgams/LinkedListPrograms/P4.java
@@ -0,0 +1,27 @@
+package JavaCollectionsProgams.LinkedListPrograms;
+
+import org.junit.Test;
+
+import java.util.LinkedList;
+
+public class P4 {
+
+    @Test
+    public void numLinkedList(){
+        System.out.println(" ");
+        System.out.println("###################### START ######################");
+
+        LinkedList<Integer> numList = new LinkedList<>();
+        numList.add(5);
+        numList.add(77);
+        numList.add(32);
+        numList.add(3);
+
+        numList.addFirst(1);
+        numList.addLast(89);
+
+        System.out.println("Linked List is: " + numList);
+
+    }
+
+}
