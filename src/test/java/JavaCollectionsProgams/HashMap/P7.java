a224a83b4cfc9c510c537dd1f06cd5d0f487087b
Utkarsh Karnwal <utkarsheons@gmail.com>
2026-02-23 16:31:25 +0530

    commit

---
+++ src/test/java/JavaCollectionsProgams/HashMap/P7.java
@@ -0,0 +1,42 @@
+package JavaCollectionsProgams.HashMap;
+
+import org.junit.Test;
+
+import java.util.*;
+
+public class P7 {
+
+    @Test
+    public void testOccurancesInAString(){
+        System.out.println(" ");
+        System.out.println("###################### START P7 ######################");
+
+        String strMain= "panama canal";
+        System.out.println("String is: " + strMain);
+        String str = strMain.replace(" ", "");
+
+        Set<Character> charSet = new HashSet<>();
+        for(char a:str.toCharArray()){
+            charSet.add(a);
+        }
+        System.out.println("CharSet is: " + charSet);
+
+        Map<Character,Integer> charMap = new HashMap<>();
+        int counter=0;
+        Iterator<Character> it = charSet.iterator();
+        while(it.hasNext()){
+            char a = it.next();
+            for(int i=0;i<str.length();i++){
+                if(a==str.charAt(i)){
+                    counter++;
+                }
+            }
+            charMap.put(a,counter);
+            counter=0;
+        }
+
+        System.out.println("Occurances are: " + charMap);
+
+    }
+
+}
